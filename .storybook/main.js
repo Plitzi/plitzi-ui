94cb07b563d328fb45cad94189e38457bcecc991
Carlos Rodrigues <carlos_eduardo309@hotmail.com>
2022-12-12 20:38:20 +1100

    changes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,3 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const webpack = require('webpack');
-const PACKAGE = require('../package.json');
-
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-controls'],
