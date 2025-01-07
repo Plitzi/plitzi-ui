431d3819d1fc23766faa2749f381739c969e8723
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2025-01-07 22:44:18 +1100

    changes in switch

--- src/components/Switch/Switch.styles.ts
+++ src/components/Switch/Switch.styles.ts
@@ -36,7 +36,7 @@ export default {
         primary: ''
       },
       size: {
-        lg: 'w-14 h-8',
+        lg: 'w-12 h-7',
         md: 'w-10 h-6',
         sm: 'w-8 h-5',
         xs: 'w-6 h-4',
@@ -60,7 +60,7 @@ export default {
           primary: 'bg-gray-300 peer-checked:bg-primary-500'
         },
         size: {
-          lg: 'before:w-6 before:h-6 peer-checked:before:translate-x-[24px]',
+          lg: 'before:w-5 before:h-5 peer-checked:before:translate-x-[20px]',
           md: 'before:w-4 before:h-4 peer-checked:before:translate-x-[16px]',
           sm: 'before:w-3 before:h-3 peer-checked:before:translate-x-[12px]',
           xs: 'before:w-2 before:h-2 peer-checked:before:translate-x-[8px]',
