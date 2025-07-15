dd3e727cd40e5a3e15b3b7a9ed3bdebfac1bbc72
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2025-07-15 16:03:51 +0100

    missing exports

--- src/components/Modal/index.ts
+++ src/components/Modal/index.ts
@@ -1,5 +1,13 @@
 import Modal from './Modal';
+import ModalContext from './ModalContext';
+import ModalProvider from './ModalProvider';
+import useModal from './useModal';
 
 export * from './Modal';
+export * from './useModal';
+export * from './ModalProvider';
+export * from './ModalContext';
+
+export { useModal, ModalProvider, ModalContext };
 
 export default Modal;
