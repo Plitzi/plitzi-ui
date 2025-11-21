0544d05a8c5d9d7d532bfeae12a98d3396fc4cfc
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2025-11-21 19:08:35 +0000

    v1.1.2

--- src/components/Markdown/Markdown.tsx
+++ src/components/Markdown/Markdown.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import omit from 'lodash/omit.js';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism/index.js';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 
