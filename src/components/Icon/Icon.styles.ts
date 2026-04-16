cdd4b7a2bf144c249de381f227270f747ff4f9af
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2026-04-16 13:47:09 +0100

    some comments

--- src/components/Icon/Icon.styles.ts
+++ src/components/Icon/Icon.styles.ts
@@ -40,19 +40,19 @@ export default {
         custom: ''
       },
       size: {
-        xs: 'text-xs',
-        sm: 'text-sm',
-        md: '',
-        lg: 'text-lg',
-        xl: 'text-xl',
-        '2xl': 'text-2xl',
-        '3xl': 'text-3xl',
-        '4xl': 'text-4xl',
-        '5xl': 'text-5xl',
-        '6xl': 'text-6xl',
-        '7xl': 'text-7xl',
-        '8xl': 'text-8xl',
-        '9xl': 'text-9xl',
+        xs: 'text-xs', // w-3 h-3
+        sm: 'text-sm', // w-3.5 h-3.5
+        md: '', // w-4 h-4
+        lg: 'text-lg', // w-4.5 h-4.5
+        xl: 'text-xl', // w-6 h-6
+        '2xl': 'text-2xl', // w-7 h-7
+        '3xl': 'text-3xl', // w-8 h-8
+        '4xl': 'text-4xl', // w-9 h-9
+        '5xl': 'text-5xl', // w-12 h-12
+        '6xl': 'text-6xl', // w-16 h-16
+        '7xl': 'text-7xl', // w-18 h-18
+        '8xl': 'text-8xl', // w-24 h-24
+        '9xl': 'text-9xl', // w-32 h-32
         custom: ''
       }
     },
